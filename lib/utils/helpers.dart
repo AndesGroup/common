ff7b96366594cda3148e86a473fb3927eb22f406
tbm98 <tbm98.dev@gmail.com>
2022-03-09 16:39:57 +0700

    more

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
 import 'package:validators/validators.dart';
 
 import '../config/const.dart';
@@ -25,3 +28,74 @@ MediaType getMediaType(String? mediaSrc) {
     throw Exception('Invalid Media Type');
   }
 }
+T getRandomElement<T>(List<T> list) {
+  final random = Random();
+  var i = random.nextInt(list.length);
+  return list[i];
+}
+///////PARSER////////////
+List<T?>? parseList<T extends Object?>({
+  required List<dynamic>? json,
+  required T Function(Map<String, dynamic> json) fromJson,
+}) {
+  return (json)?.map((e) => e == null ? null : fromJson(e as Map<String, dynamic>)).toList();
+}
+
+List<T> parseListNotNull<T extends Object?>({
+  required List<dynamic> json,
+  required T Function(Map<String, dynamic> json) fromJson,
+}) {
+  return (json).map((e) => fromJson(e as Map<String, dynamic>)).toList();
+}
+
+Map<String, T?> parseMap<T extends Object?>({
+  required Map<String, dynamic> json,
+  required T Function(Map<String, dynamic> json) fromJson,
+}) {
+  return json.map((k, e) => MapEntry(k, e == null ? null : fromJson(e as Map<String, dynamic>)));
+}
+
+Map<String, T> parseMapNotNull<T extends Object?>({
+  required Map<String, dynamic> json,
+  required T Function(Map<String, dynamic> json) fromJson,
+}) {
+  return json.map((k, e) => MapEntry(k, fromJson(e as Map<String, dynamic>)));
+}
+////////////////////////
+
+///////Transition////////////
+Offset offsetFromDirection(AxisDirection axisDirection) {
+  switch (axisDirection) {
+    case AxisDirection.up:
+      return const Offset(0.0, 1.0);
+    case AxisDirection.right:
+      return const Offset(1.0, 0.0);
+    case AxisDirection.down:
+      return const Offset(0.0, -1.0);
+    case AxisDirection.left:
+      return const Offset(-1.0, 0.0);
+  }
+}
+
+Route defaultPage(Widget child) {
+  return MaterialPageRoute(builder: (_) => child);
+}
+
+Route slidePage(Widget child, {AxisDirection axisDirection = AxisDirection.right}) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      final begin = offsetFromDirection(axisDirection);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+/////////////////////////////
