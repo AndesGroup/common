9bce698490febb13ce728031051cb6f46dce90ff
tbm98 <tbm98.dev@gmail.com>
2022-07-29 12:49:08 +0700

    update 1.0.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.9
+* add OnDateTime extension
+
 ## 1.0.8
 * add TimeOfDayConverterNotNull
 
--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,3 +1,4 @@
+import 'package:andesgroup_common/common.dart';
 import 'package:flutter/material.dart';
 
 extension OnTimeOfDay on TimeOfDay {
@@ -7,9 +8,14 @@ extension OnTimeOfDay on TimeOfDay {
 }
 
 extension OnDuration on Duration {
-
   /// convert [Duration] to [TimeOfDay], only hour and minute will be keep.
   TimeOfDay get timeOfDay {
     return TimeOfDay(hour: inHours % 24, minute: inMinutes % 60);
+  }
+}
+
+extension OnDateTime on DateTime {
+  String get formatDDMMYYYY {
+    return DateFormat('dd/MM/yyyy').format(this);
   }
 }
